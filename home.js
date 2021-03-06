a3c8bf01cd654f0440c15eeb3b65a51c088be1e9
NickDempseyDev <nickdempseydev@gmail.com>
2021-03-06 01:01:45 +0000

    Added home.html/css/js with initial (incomplete) concept design

---
+++ home.js
@@ -0,0 +1,18 @@
+function openNav() {
+  var client_width = document.documentElement.clientWidth;
+  if (client_width <= 1200) {
+    document.getElementById("mySidebar").style.width = "30%";
+    document.getElementById("main").style.marginLeft = "30%";
+    document.getElementById("file-upload-container").style.marginLeft = "20%";
+  } else {
+    document.getElementById("mySidebar").style.width = "10%";
+    document.getElementById("main").style.marginLeft = "10%";
+    // document.getElementById("file-upload-container").style.marginLeft = "10%";
+  }
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("file-upload-container").style.marginLeft = "0%";
+}
